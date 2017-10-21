0ccdb206d15a2e0030ba87b0d7c5684164865b4b
Jacob Casper <jacob.thomas.casper@gmail.com>
2017-10-21 16:04:01 -0400

    Created timer with Button Start/pause

--- BoxTimer/ViewController.swift
+++ BoxTimer/ViewController.swift
@@ -9,16 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var countdownTimer: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var minutes = 3
+    var seconds = 0
+    var running = false
+    var timer: Timer!
+    
+    let dateFormatter = DateFormatter()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        dateFormatter.dateFormat = "mm:ss"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Actions
+    @IBAction func startPause(_ sender: UIButton) {
+        if !running {
+            // Timer is paused or stopped
+            startButton.setTitle("Pause", for: UIControlState.normal)
+            running = true
+            let timerDate = dateFormatter.date(from: "\(minutes):\(seconds)")
+            countdownTimer.text = dateFormatter.string(from: timerDate!)
+            countdownTimer.alpha = 1.0
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.decrementTime), userInfo: nil, repeats: true)
+        } else {
+            // Timer is running; pause it
+            startButton.setTitle("Start the Clock", for: UIControlState.normal)
+            running = false
+            timer.invalidate()
+        }
+    }
+    
+    //MARK: Private Functions
+    @objc private func decrementTime() {
+        if seconds == 0 && minutes == 0 {
+            startButton.backgroundColor = UIColor.red
+        } else if seconds == 0 {
+            minutes -= 1
+            seconds = 59
+        } else {
+            seconds -= 1
+        }
+        let timerDate = dateFormatter.date(from: "\(minutes):\(seconds)")
+        countdownTimer.text = dateFormatter.string(from: timerDate!)
+    }
+    
 
 
 }
